c307d975bc3f70b1ef6c8e01de479c865abc0f55
Štěpán Havrlant <Stepan.Havrlant@alza.cz>
2025-03-04 19:57:17 +0100

    Clean up using directives and refactor code organization
    - Reduced unused `using` directives in `Program.cs` and `StaticObjects.cs`.
    - Wrapped `CheckForUpdates` in `#if !DEBUG` in `Program.cs`.
    - Organized `UserSettings.cs` with `#region` for constants and properties.

--- QaseTestCaseGenerator/Settings/UserSettings.cs
+++ QaseTestCaseGenerator/Settings/UserSettings.cs
@@ -8,13 +8,17 @@ namespace QaseTestCaseGenerator.Settings
 {
     public class UserSettings
     {
+        #region Consts
+        public const string ProfileDirectory = "Profiles";
+        #endregion
+
+        #region Props
         public static string? Adm2AuthCookie { get; set; }
         public static string? JsessionIdCookie { get; set; }
         public static string? OpenAIApiKey { get; set; }
         public static string? QaseApiToken { get; set; }
         public static string UserTestCaseDirectory { get; set; } = "TestCases";
         public static string OpenAIModel { get; set; } = "gpt-3.5-turbo";
-        public const string ProfileDirectory = "Profiles";
-        
+        #endregion
     }
 }
